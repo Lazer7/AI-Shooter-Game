aa00ca62f9df6d9ca8b3e0f43844013fa41406bb
James <iamjimmyjimmy@yahoo.com>
2017-11-17 14:17:15 -0800

    Adding Enemy Movement Definition

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,66 +4,72 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     int count;
+
+    public Queue<Direction> playerMovement;
 	// Use this for initialization
+    
 	void Start () {
         count = 0;
+        playerMovement = new Queue<Direction>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        GameObject bullet;
-       
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            bullet = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-            bullet.AddComponent<Rigidbody>();
-            bullet.transform.Translate(this.transform.position.x, this.transform.position.y, this.transform.position.z);
-            bullet.GetComponent<Rigidbody>().useGravity = false;
-            bullet.GetComponent<CapsuleCollider>().enabled = false;
-            bullet.GetComponent<Rigidbody>().AddForce(0f, 100f, 0f);
-            count++;
-            print(count);
+            createBullet(0, -100, 0, 180);
             transform.Translate(0f, 1f, 0f);
+            updateQueue(Direction.up);
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
-            bullet = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-            bullet.AddComponent<Rigidbody>();
-            bullet.transform.Translate(this.transform.position.x, this.transform.position.y, this.transform.position.z);
-            bullet.transform.Rotate(0, 0, 180);
-            bullet.GetComponent<Rigidbody>().useGravity = false;
-            bullet.GetComponent<CapsuleCollider>().enabled = false;
-            bullet.GetComponent<Rigidbody>().velocity = new Vector3(0, (-100f), 0);
-            count++;
-            print(count);
+            createBullet(0, -100, 0, 180);
             transform.Translate(0f, -1f, 0f);
+            updateQueue(Direction.down);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            bullet = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-            bullet.AddComponent<Rigidbody>();
-            bullet.transform.Translate(this.transform.position.x+2, this.transform.position.y, this.transform.position.z);
-            bullet.transform.Rotate(0,0,90);
-            bullet.GetComponent<CapsuleCollider>().enabled = false;
-            bullet.GetComponent<Rigidbody>().useGravity = false;
-            bullet.GetComponent<Rigidbody>().velocity = new Vector3(100f, 0f, 0);
-            count++;
-            print(count);
+            createBullet(100, 0, 0, 90);
             transform.Translate(1f, 0f, 0f);
+            updateQueue(Direction.right);
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
-            bullet = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-            bullet.AddComponent<Rigidbody>();
-            bullet.transform.Translate(this.transform.position.x-2, this.transform.position.y, this.transform.position.z);
-            bullet.transform.Rotate(0, 0,270);
-            bullet.GetComponent<CapsuleCollider>().enabled = false;
-            bullet.GetComponent<Rigidbody>().useGravity = false;
-            bullet.GetComponent<Rigidbody>().AddForce((-100f), 0f, 0f);
-            count++;
-            print(count);
+            createBullet(-100, 0, 0, 270);
             transform.Translate(-1f, 0f, 0f);
+            updateQueue(Direction.left);
         }
+        int i = 0;
+        foreach (Direction x in playerMovement)
+        {
+            print(i+": "+x);
+            i++;
+        }
+
     }
+    void updateQueue(Direction currentPlayerMovement)
+    {
+        if (playerMovement.Count == 50)
+        {
+            playerMovement.Dequeue();
+        }
+        playerMovement.Enqueue(currentPlayerMovement);
+    }
+    void createBullet(float x, float y , float z, int rotation)
+    {
+        GameObject bullet;
+        bullet = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        bullet.transform.Translate(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+        bullet.AddComponent<Rigidbody>();
+        bullet.transform.Rotate(0, 0, rotation);
+        bullet.GetComponent<CapsuleCollider>().enabled = false;
+        bullet.GetComponent<Rigidbody>().useGravity = false;
+        bullet.GetComponent<Rigidbody>().AddForce(x,y,z);
+    }
+
 }
+
+
+
+
--- Assets/Scripts/ReflexEnemy.cs
+++ Assets/Scripts/ReflexEnemy.cs
@@ -3,14 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ReflexEnemy : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+   
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        GameObject player = GameObject.Find("PlayerObject");
+        Queue<Direction> playerMovement = player.GetComponent<Player>().playerMovement;
+        while(playerMovement != null)
+        {
+            switch (playerMovement.Peek()) {
+                case Direction.up:
+                    this.transform.Translate(0f, 1f, 0f);
+                    playerMovement.Dequeue();
+                    break;
+                case Direction.down:
+                    this.transform.Translate(0f, -1f, 0f);
+                    playerMovement.Dequeue();
+                    break;
+                case Direction.left:
+                    this.transform.Translate(-1f, 0f, 0f);
+                    playerMovement.Dequeue();
+                    break;
+                case Direction.right:
+                    this.transform.Translate(1f, 0f, 0f);
+                    playerMovement.Dequeue();
+                    break;
+
+
+
+            }
+
+        }
+       
+
+
 	}
 }
